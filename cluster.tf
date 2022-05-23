bcbdda180cd980ca65df76d2fbb3340950fae7b8
manu <manu@local.com>
2022-05-23 22:42:59 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -6,7 +6,7 @@ resource "aws_elasticache_cluster" "redis" {
   parameter_group_name = aws_elasticache_parameter_group.default.name
   engine_version       = "6.x"
   port                 = 6379
-  subnet_group_name    = "aws_elasticache_subnet_group.subnet-group.name"
+  subnet_group_name    = aws_elasticache_subnet_group.subnet-group.name
   security_group_ids  = [aws_security_group.redis.id]
 }
 
