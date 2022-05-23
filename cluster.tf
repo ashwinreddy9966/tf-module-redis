7738a047e2751468915677e26e137f50f317da93
manu <manu@local.com>
2022-05-23 22:37:33 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "redis" {
   ingress {
     description = "Allows Redis Port"
     from_port   = 6379
-    to_port     = 6370
+    to_port     = 6379
     protocol    = "-1"
     cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
   }
