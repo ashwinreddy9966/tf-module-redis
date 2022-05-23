8b45f293788e0c2f6cfd0eb29315ffca96340bcd
manu <manu@local.com>
2022-05-23 20:52:22 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "redis" {
     from_port   = 6379
     to_port     = 6370
     protocol    = "-1"
-    cidr_blocks = data.terraform_remote_state.vpc.outputs.VPC_CIDR
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
   }
 
   egress {
