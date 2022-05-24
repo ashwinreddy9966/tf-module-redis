ffc2a4e6f454434358d12f23b5adf67319f040a5
manu <manu@local.com>
2022-05-24 10:03:17 +0530

    Auto Push Commit

---
+++ route53.tf
@@ -0,0 +1,9 @@
+#resource "aws_route53_record" "record" {
+#  zone_id = data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ID
+#  name    = "redis-${var.ENV}.${data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ZONE}"
+#  type    = "CNAME"
+#  ttl     = "60"
+#  records = [aws_docdb_cluster.redis.????]
+#}
+
+
